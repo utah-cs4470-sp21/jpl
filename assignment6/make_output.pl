d7b13c47a51c7354bfe183f3faa8cf2b754ac879
John Regehr <regehr@cs.utah.edu>
2021-04-23 16:02:27 -0600

    foo

--- assignment6/make_output.pl
+++ assignment6/make_output.pl
@@ -3,14 +3,14 @@
 use strict;
 use autodie;
 
-my $JPLC = "/Users/johnregehr/compiler-class/pavpan/compile.py";
+my $JPLC = $ENV{"HOME"}."/compiler-class/pavpan/compile.py";
 
 sub go($$) {
     (my $dir, my $flag) = @_;
     die unless -d $dir;
     my @files = glob "$dir/*.jpl";
     foreach my $f (@files) {
-        system "$JPLC $flag $f";
+        system "$JPLC -t $flag $f";
     }
 }
 
